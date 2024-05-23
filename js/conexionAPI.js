adb1fa7eb25f5e40c243e172673fb609c1eefca5
Gabriel Marrero <gab.bmar.05@gmail.com>
2024-05-22 22:14:34 -0300

    commit 22-5 added function to capture form values and create new product. Also added method to insert the new product in "db".

--- js/conexionAPI.js
+++ js/conexionAPI.js
@@ -6,7 +6,20 @@ async function listProducts() {
     return conexionConvertida;
 }
 
+async function createProduct(title, image){
+    const conexion = await fetch("http://localhost:3001/products", {
+        method: "POST",
+        header:{"Content-type":"application/json"},
+        body: JSON.stringify({
+            title:title,
+            image:image
+        })
+    });
+    const conexionConvertida = conexion.json();
+
+    return conexionConvertida;
+}
 
 export const conexionAPI = {
-    listProducts
+    listProducts, createProduct
 }
---
+++ js/createProduct.js
@@ -0,0 +1,17 @@
+import { conexionAPI } from "./conexionAPI.js";
+
+const form = document.querySelector("[data-form]");
+
+async function createCard(event){
+    event.preventDefault();
+
+    const name = document.querySelector("[data-title]").value;
+    const image = document.querySelector("[data-img]").value;
+
+    await conexionAPI.createProduct(name, image);
+
+    window.location.href = "../index.html";
+}
+
+
+form.addEventListener("submit", event => createCard(event));
